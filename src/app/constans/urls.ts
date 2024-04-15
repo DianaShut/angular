b2822e1306a435ca669d399abfe613028b882c84
DianaShut <146943634+DianaShut@users.noreply.github.com>
2024-04-15 16:10:35 +0300

    .

---
+++ src/app/constans/urls.ts
@@ -0,0 +1,14 @@
+const API = 'https://jsonplaceholder.typicode.com'
+
+const users  = `${API}/users`
+const posts = `${API}/posts`
+
+const urls = {
+  users:{
+    base: users,
+    byId: (id:number): string => `${users}/${id}`
+  },
+  posts
+}
+
+export {urls}
